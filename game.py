1622452ffa4d6ef9f0dd0eff8fd72f42d2e4fff1
yascha <yascha@gmail.com>
2013-11-20 22:30:06 -0800

    Add the beginnings of a method to check if a move is valid or not.

--- game.py
+++ game.py
@@ -31,8 +31,11 @@ class Board(object):
             for col in self.columns:
                 boardRow += " "*spacing + Colours.chars[col[row]]
             print boardRow + "\n"
+            # TODO: Look into actually printing each char in colour.
+            # There are lots of python libraries that do cross-platform
+            # ANSI colouring of text
 
-    def isValidMove(self, coordsList):
+    def _isValidMove(self, coordsList):
         """ 
         Check if the requested move is valid.
         Returns true if the move is valid, false otherwise.
@@ -41,6 +44,60 @@ class Board(object):
                 where (0,0) is the bottom left corner of the board.
         """
         
+        # For a move to be valid, all dots must be the same colour,
+        # the path must not cross the same dot more than twice,
+        # and the same path between two dots cannot be traversed more
+        # than once.
+        
+        # Make sure all the coordinates are valid
+        for (x,y) in coordsList:
+            if not self._areValidCoords(x,y):
+                return False
+        
+        # Make sure all the dots are the same colour
+        firstDotColour = self._getColour(coordsList[0][0], coordsList[0][1])
+        if not all(firstDotColour == self._getColour(coords[0], coords[1]) 
+                   for coords in coordsList):
+            return False
+        
+        # Now make sure they are all connected
+        
+        
+        
+        return True
+    
+    def _getColour(self, xcoords, ycoords):
+        """
+        Gets the numerical colour (from Colours) of the dot at the 
+        specified coordinates where (0,0) is the bottom left corner
+        of the board.
+        Assumes the coordinates are valid.
+        """
+        
+        return self.columns[xcoords][ycoords]
+
+    def _areValidCoords(self, xcoords, ycoords):
+        """
+        Checks if the coordinates are valid in the coordinate system where 
+        (0,0) is the bottom left corner of the board.
+        Returns true if so, false otherwise.
+        """
+        if (xcoords == None or ycoords == None):
+            return False
+        if not (xcoords >= 0 and xcoords < self.numColumns and 
+                ycoords >= 0 and ycoords < self.numRows):
+            return False
+        return True
+
+
+    def _isNeighbour(self, firstDotCoords, secondDotCoords):
+        """
+        Checks if the two passed in locations are vertical or
+        horizontal neighbours.
+        Returns true if so, false otherwise.
+        """
+        # TODO: Implement this
+        return True
 
 
 class Colours:
@@ -59,4 +116,3 @@ class Colours:
                 YELLOW : 'y',
                 PURPLE : 'p'
             } 
-
